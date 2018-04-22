11a9ebf589c831c355df8ea6338b740c7ea4da7d
Kondo-Totori <hwg_song_0f_suim@yahoo.co.jp>
2018-04-22 12:02:48 +0900

    Car position.Z based on Main Camera

--- Project/Hachitai/Assets/MouseTracking.cs
+++ Project/Hachitai/Assets/MouseTracking.cs
@@ -7,10 +7,11 @@ public class MouseTracking : MonoBehaviour {
     private Vector3 position;
     // スクリーン座標をワールド座標に変換した位置座標
     private Vector3 screenToWorldPointPosition;
+    private GameObject cam;
     // Use this for initialization
     void Start()
     {
-
+        cam = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
@@ -19,7 +20,7 @@ public class MouseTracking : MonoBehaviour {
         // Vector3でマウス位置座標を取得する
         position = Input.mousePosition;
         // Z軸修正
-        position.z = 10f;
+        position.z = cam.transform.position.z+20;
         // マウス位置座標をスクリーン座標からワールド座標に変換する
         screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
         // ワールド座標に変換されたマウス座標を代入
