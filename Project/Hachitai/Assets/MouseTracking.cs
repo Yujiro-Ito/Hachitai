07df6a3857ddde95e985353e335455271bfa5275
Kondo-Totori <hwg_song_0f_suim@yahoo.co.jp>
2018-04-22 12:37:12 +0900

    Mouse Tracking Function Completed

--- Project/Hachitai/Assets/MouseTracking.cs
+++ Project/Hachitai/Assets/MouseTracking.cs
@@ -8,6 +8,7 @@ public class MouseTracking : MonoBehaviour {
     // スクリーン座標をワールド座標に変換した位置座標
     private Vector3 screenToWorldPointPosition;
     private GameObject cam;
+    public float euclidean_distance;
     // Use this for initialization
     void Start()
     {
@@ -23,7 +24,10 @@ public class MouseTracking : MonoBehaviour {
         position.z = cam.transform.position.z+20;
         // マウス位置座標をスクリーン座標からワールド座標に変換する
         screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
-        // ワールド座標に変換されたマウス座標を代入
-        gameObject.transform.position = screenToWorldPointPosition;
+        // ワールド座標に変換されたマウス座標と車のユークリッド距離を計算する
+        euclidean_distance = Mathf.Sqrt(Mathf.Pow(screenToWorldPointPosition.x - transform.position.x, 2) + Mathf.Pow(screenToWorldPointPosition.y - transform.position.y, 2));
+        // 車をマウスに追従させる
+        //gameObject.transform.position = screenToWorldPointPosition;
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(screenToWorldPointPosition.x, screenToWorldPointPosition.y), (4*euclidean_distance) * Time.deltaTime);
     }
 }
