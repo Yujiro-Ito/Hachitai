97ef9752cae2608410822f0345095961789a3abd
Kondo-Totori <hwg_song_0f_suim@yahoo.co.jp>
2018-04-22 12:52:38 +0900

    Generate Enemy proto comitted

--- Project/Hachitai/Assets/GenerateEnemy.cs
+++ Project/Hachitai/Assets/GenerateEnemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class GenerateEnemy : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
